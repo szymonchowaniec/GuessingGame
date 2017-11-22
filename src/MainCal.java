aa956d9e85e26f9b7a65d6248d8db3bef85b360b
Justyna-TOSHIBA\Justyna <szymonchowaniec@gmail.com>
2017-11-22 11:49:24 +0100

    Initial commit

---
+++ src/MainCal.java
@@ -0,0 +1,7 @@
+
+public class MainCal {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.start();
+    }
+}
---
+++ src/UtilsWords.java
@@ -0,0 +1,3 @@
+public class UtilsWords {
+    public static final String WORDS = "Mam nadzieje że jak ktoś tu dotrze i to zobaczy to się uśmiechnie";
+}
